86bb197f35f7fe2f3b68e9595fda98f36bb71fc4
Jack Barrett <jjbdev21@gmail.com>
2023-07-29 11:45:46 +0100

    Initialize app

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import Resizer from "./Resizer"
+
+function App() {
+  return (
+    <div className="w-full h-full p-4 flex flex-col items-center justify-center">
+      <h1>Image Resizer</h1>
+    </div>
+  )
+}
+
+export default App
